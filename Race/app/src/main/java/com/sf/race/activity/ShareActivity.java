5f0e3675ac8aabee4377bcb7a1c36648b0ebd493
jcj <379362778@qq.com>
2017-12-15 14:05:45 +0800

    完成微信分享的dialog

--- Race/app/src/main/java/com/sf/race/activity/ShareActivity.java
+++ Race/app/src/main/java/com/sf/race/activity/ShareActivity.java
@@ -2,8 +2,11 @@ package com.sf.race.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.sf.race.R;
+import com.sf.race.view.ShareDialog;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -11,5 +14,6 @@ public class ShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+        Button share = (Button) findViewById(R.id.bt_share);
     }
 }
---
+++ Race/app/src/main/java/com/sf/race/view/ShareDialog.java
@@ -0,0 +1,70 @@
+package com.sf.race.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.sf.race.R;
+
+/**
+ * Created by jcj on 2017/12/15.
+ */
+
+public class ShareDialog extends Dialog{
+
+    public ShareDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    public ShareDialog(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    protected ShareDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.share_dialog);
+        Button shareWechat = (Button) findViewById(R.id.share_wechat);
+        Button shareWechatGroup = (Button) findViewById(R.id.share_wechat_group);
+        shareWechat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener !=null) {
+                    listener.clickShareWechat();
+                }
+            }
+        });
+        shareWechatGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.clickShareWechatGroup();
+                }
+            }
+        });
+    }
+    private ShareWechatListener listener;
+
+    public ShareWechatListener getListener() {
+        return listener;
+    }
+
+    public void setListener(ShareWechatListener listener) {
+        this.listener = listener;
+    }
+
+    interface ShareWechatListener{
+        void clickShareWechat();
+        void clickShareWechatGroup();
+    }
+}
