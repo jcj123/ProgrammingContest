65a51fc353e153c2fd6eb4d05ce01a798c61c922
luxb <1332975243@qq.com>
2017-12-15 16:57:03 +0800

    Merge branch 'master' of https://github.com/jcj123/ProgrammingContest
    # Conflicts:
    #	Race/app/src/main/java/com/sf/race/Utils/OkHttp3Util.java

---
+++ Race/app/src/main/java/com/sf/race/bean/MassUser.java
@@ -0,0 +1,67 @@
+package com.sf.race.bean;
+public class MassUser {
+
+    private String massId;
+    private String province;
+    private String address;
+    private String userName;
+    private String phone;
+    private String sendNumber;
+    private String sendWeight;
+
+    public String getMassId() {
+        return massId;
+    }
+
+    public void setMassId(String massId) {
+        this.massId = massId;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSendNumber() {
+        return sendNumber;
+    }
+
+    public void setSendNumber(String sendNumber) {
+        this.sendNumber = sendNumber;
+    }
+
+    public String getSendWeight() {
+        return sendWeight;
+    }
+
+    public void setSendWeight(String sendWeight) {
+        this.sendWeight = sendWeight;
+    }
+}
