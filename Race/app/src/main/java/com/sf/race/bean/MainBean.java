3b851217e50f85748fd9f6062d591e44a3df26ea
小屁孩勾勾 <1477901583@qq.com>
2017-12-15 17:07:24 +0800

    bean

--- Race/app/src/main/java/com/sf/race/bean/MainBean.java
+++ Race/app/src/main/java/com/sf/race/bean/MainBean.java
@@ -8,113 +8,204 @@ import java.util.List;
  */
 
 public class MainBean {
-    public String getPicUrl() {
-        return picUrl;
+
+    /**
+     * success : true
+     * status : 0
+     * message : null
+     * obj : {"id":"2c029290-e16b-11e7-83f0-00ff8ba56b27","mktId":"e174424f-e153-11e7-83f0-00ff8ba56b27","lowestPrice":"6元/1.5KG","lowestFreight":"120","picUrl":"/cc","massType":"全國團","endtm":1513327213000,"currentUsers":0,"usersPic":[],"mktNameShow":"吃鸡小队","dailyMinPackages":20,"weightMin":1.5,"weightMax":5,"groupLimit":5,"useRequire":"1.每日承诺寄件量不低于20件（周六日及法定假期不要求承诺量）日发件不足20件或运费不足120元将按照120元收取最低运作费用。\\n2.承诺使用周期内提前终止寄件视为违约行为。\\n3.集货周期内未达到目标则自动取消集货。\\n4.用户需自行完成打包并贴好面单。\\n5.每日在约定时间内进行一次交件，不支持随寄随收。\\n6.按月结算运费，不支持现寄现结。"}
+     */
+
+    private boolean success;
+    private int status;
+    private Object message;
+    private ObjBean obj;
+
+    public boolean isSuccess() {
+        return success;
     }
 
-    public String getMassType() {
-        return massType;
+    public void setSuccess(boolean success) {
+        this.success = success;
     }
 
-    public String getEndtm() {
-        return endtm;
+    public int getStatus() {
+        return status;
     }
 
-    public Integer getCurrentUsers() {
-        return currentUsers;
+    public void setStatus(int status) {
+        this.status = status;
     }
 
-    public List<String> getUsersPic() {
-        return usersPic;
+    public Object getMessage() {
+        return message;
     }
 
-    public String getMktNameShow() {
-        return mktNameShow;
+    public void setMessage(Object message) {
+        this.message = message;
     }
 
-    public Short getDailyMinPackages() {
-        return dailyMinPackages;
+    public ObjBean getObj() {
+        return obj;
     }
 
-    public String getWeightMax() {
-        return weightMax;
+    public void setObj(ObjBean obj) {
+        this.obj = obj;
     }
 
-    public String getWeightMin() {
-        return weightMin;
-    }
+    public static class ObjBean {
+        /**
+         * id : 2c029290-e16b-11e7-83f0-00ff8ba56b27
+         * mktId : e174424f-e153-11e7-83f0-00ff8ba56b27
+         * lowestPrice : 6元/1.5KG
+         * lowestFreight : 120
+         * picUrl : /cc
+         * massType : 全國團
+         * endtm : 1513327213000
+         * currentUsers : 0
+         * usersPic : []
+         * mktNameShow : 吃鸡小队
+         * dailyMinPackages : 20
+         * weightMin : 1.5
+         * weightMax : 5.0
+         * groupLimit : 5
+         * useRequire : 1.每日承诺寄件量不低于20件（周六日及法定假期不要求承诺量）日发件不足20件或运费不足120元将按照120元收取最低运作费用。\n2.承诺使用周期内提前终止寄件视为违约行为。\n3.集货周期内未达到目标则自动取消集货。\n4.用户需自行完成打包并贴好面单。\n5.每日在约定时间内进行一次交件，不支持随寄随收。\n6.按月结算运费，不支持现寄现结。
+         */
 
-    public Short getGroupLimit() {
-        return groupLimit;
-    }
+        private String id;
+        private String mktId;
+        private String lowestPrice;
+        private String lowestFreight;
+        private String picUrl;
+        private String massType;
+        private long endtm;
+        private int currentUsers;
+        private String mktNameShow;
+        private int dailyMinPackages;
+        private double weightMin;
+        private double weightMax;
+        private int groupLimit;
+        private String useRequire;
+        private List<?> usersPic;
 
-    public String getUseRequire() {
-        return useRequire;
-    }
+        public String getId() {
+            return id;
+        }
 
-    //最低價格
-    private String lowestPrice;
+        public void setId(String id) {
+            this.id = id;
+        }
 
-    //最低費用
-    private String lowestFreight;
+        public String getMktId() {
+            return mktId;
+        }
 
-    //推廣圖片
-    private String picUrl;
+        public void setMktId(String mktId) {
+            this.mktId = mktId;
+        }
 
-    //集貨類型
-    private String massType;
+        public String getLowestPrice() {
+            return lowestPrice;
+        }
 
-    public String getLowestPrice() {
-        return lowestPrice;
-    }
+        public void setLowestPrice(String lowestPrice) {
+            this.lowestPrice = lowestPrice;
+        }
 
-    //截止時間
-    private String endtm;
+        public String getLowestFreight() {
+            return lowestFreight;
+        }
 
-    //當前人數
-    private Integer currentUsers;
+        public void setLowestFreight(String lowestFreight) {
+            this.lowestFreight = lowestFreight;
+        }
 
-    //當前人數
-    private List<String> usersPic;
+        public String getPicUrl() {
+            return picUrl;
+        }
 
-    //集貨名稱
-    private String mktNameShow;
+        public void setPicUrl(String picUrl) {
+            this.picUrl = picUrl;
+        }
 
-    //最低寄件量
-    private Short dailyMinPackages;
+        public String getMassType() {
+            return massType;
+        }
 
-    //最小重量
-    private String weightMin;
+        public void setMassType(String massType) {
+            this.massType = massType;
+        }
 
-    //最大重量
-    private String weightMax;
+        public long getEndtm() {
+            return endtm;
+        }
 
-    //總名額
-    private Short groupLimit;
+        public void setEndtm(long endtm) {
+            this.endtm = endtm;
+        }
 
-    //使用要求
-    private String useRequire;
+        public int getCurrentUsers() {
+            return currentUsers;
+        }
 
-    public String getLowestFreight() {
-        return lowestFreight;
-    }
+        public void setCurrentUsers(int currentUsers) {
+            this.currentUsers = currentUsers;
+        }
 
-    @Override
-    public String toString() {
-        return "MainBean{" +
-                "lowestPrice='" + lowestPrice + '\'' +
-                ", lowestFreight='" + lowestFreight + '\'' +
-                ", picUrl='" + picUrl + '\'' +
-                ", massType='" + massType + '\'' +
-                ", endtm='" + endtm + '\'' +
-                ", currentUsers=" + currentUsers +
-                ", usersPic=" + usersPic +
-                ", mktNameShow='" + mktNameShow + '\'' +
-                ", dailyMinPackages=" + dailyMinPackages +
-                ", weightMin='" + weightMin + '\'' +
-                ", weightMax='" + weightMax + '\'' +
-                ", groupLimit=" + groupLimit +
-                ", useRequire='" + useRequire + '\'' +
-                '}';
+        public String getMktNameShow() {
+            return mktNameShow;
+        }
+
+        public void setMktNameShow(String mktNameShow) {
+            this.mktNameShow = mktNameShow;
+        }
+
+        public int getDailyMinPackages() {
+            return dailyMinPackages;
+        }
+
+        public void setDailyMinPackages(int dailyMinPackages) {
+            this.dailyMinPackages = dailyMinPackages;
+        }
+
+        public double getWeightMin() {
+            return weightMin;
+        }
+
+        public void setWeightMin(double weightMin) {
+            this.weightMin = weightMin;
+        }
+
+        public double getWeightMax() {
+            return weightMax;
+        }
+
+        public void setWeightMax(double weightMax) {
+            this.weightMax = weightMax;
+        }
+
+        public int getGroupLimit() {
+            return groupLimit;
+        }
+
+        public void setGroupLimit(int groupLimit) {
+            this.groupLimit = groupLimit;
+        }
+
+        public String getUseRequire() {
+            return useRequire;
+        }
+
+        public void setUseRequire(String useRequire) {
+            this.useRequire = useRequire;
+        }
+
+        public List<?> getUsersPic() {
+            return usersPic;
+        }
+
+        public void setUsersPic(List<?> usersPic) {
+            this.usersPic = usersPic;
+        }
     }
 }
