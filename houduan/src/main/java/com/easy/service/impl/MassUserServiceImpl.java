ef0f4e599e81a122ce607ea572b11aa59db3f17b
luxing <lxcurtain@gmail.com>
2017-12-15 15:27:55 +0800

    新增 加入用户

--- houduan/src/main/java/com/easy/service/impl/MassUserServiceImpl.java
+++ houduan/src/main/java/com/easy/service/impl/MassUserServiceImpl.java
@@ -8,6 +8,7 @@ import com.easy.domain.Mass;
 import com.easy.domain.MassUser;
 import com.easy.domain.ProMarket;
 import com.easy.service.MassUserService;
+import java.util.Date;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,6 +37,9 @@ public class MassUserServiceImpl implements MassUserService {
     //集货信息是否已经过时
     Mass mass = massMapper.selectByPrimaryKey(massId);
     if (mass == null) {
+      return 0;
+    }
+    if (new Date().after(mass.getEndtm())){
       return 0;
     }
     //集货人员是否已经满
