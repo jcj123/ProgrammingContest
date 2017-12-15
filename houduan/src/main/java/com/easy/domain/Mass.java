d4252c45e387f0f9f801808d3a2b88d0d0af101a
luxing <lxcurtain@gmail.com>
2017-12-15 11:43:48 +0800

    修改

---
+++ houduan/src/main/java/com/easy/domain/Mass.java
@@ -0,0 +1,85 @@
+package com.easy.domain;
+
+import java.util.Date;
+
+public class Mass {
+    private String id;
+
+    private String mktId;
+
+    private String lowestPrice;
+
+    private String lowestFreight;
+
+    private String picUrl;
+
+    private String massType;
+
+    private Date endtm;
+
+    private Integer currentUsers;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMktId() {
+        return mktId;
+    }
+
+    public void setMktId(String mktId) {
+        this.mktId = mktId;
+    }
+
+    public String getLowestPrice() {
+        return lowestPrice;
+    }
+
+    public void setLowestPrice(String lowestPrice) {
+        this.lowestPrice = lowestPrice;
+    }
+
+    public String getLowestFreight() {
+        return lowestFreight;
+    }
+
+    public void setLowestFreight(String lowestFreight) {
+        this.lowestFreight = lowestFreight;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getMassType() {
+        return massType;
+    }
+
+    public void setMassType(String massType) {
+        this.massType = massType;
+    }
+
+    public Date getEndtm() {
+        return endtm;
+    }
+
+    public void setEndtm(Date endtm) {
+        this.endtm = endtm;
+    }
+
+    public Integer getCurrentUsers() {
+        return currentUsers;
+    }
+
+    public void setCurrentUsers(Integer currentUsers) {
+        this.currentUsers = currentUsers;
+    }
+}
---
+++ houduan/src/main/java/com/easy/domain/ProMarket.java
@@ -0,0 +1,105 @@
+package com.easy.domain;
+
+import java.math.BigDecimal;
+
+public class ProMarket {
+    private String mktId;
+
+    private String mktNameShow;
+
+    private Short dailyMinPackages;
+
+    private BigDecimal weightMin;
+
+    private BigDecimal weightMax;
+
+    private BigDecimal basePrice;
+
+    private BigDecimal baseWeight;
+
+    private Short groupLimit;
+
+    private Byte groupDuration;
+
+    private String useRequire;
+
+    public String getMktId() {
+        return mktId;
+    }
+
+    public void setMktId(String mktId) {
+        this.mktId = mktId;
+    }
+
+    public String getMktNameShow() {
+        return mktNameShow;
+    }
+
+    public void setMktNameShow(String mktNameShow) {
+        this.mktNameShow = mktNameShow;
+    }
+
+    public Short getDailyMinPackages() {
+        return dailyMinPackages;
+    }
+
+    public void setDailyMinPackages(Short dailyMinPackages) {
+        this.dailyMinPackages = dailyMinPackages;
+    }
+
+    public BigDecimal getWeightMin() {
+        return weightMin;
+    }
+
+    public void setWeightMin(BigDecimal weightMin) {
+        this.weightMin = weightMin;
+    }
+
+    public BigDecimal getWeightMax() {
+        return weightMax;
+    }
+
+    public void setWeightMax(BigDecimal weightMax) {
+        this.weightMax = weightMax;
+    }
+
+    public BigDecimal getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(BigDecimal basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public BigDecimal getBaseWeight() {
+        return baseWeight;
+    }
+
+    public void setBaseWeight(BigDecimal baseWeight) {
+        this.baseWeight = baseWeight;
+    }
+
+    public Short getGroupLimit() {
+        return groupLimit;
+    }
+
+    public void setGroupLimit(Short groupLimit) {
+        this.groupLimit = groupLimit;
+    }
+
+    public Byte getGroupDuration() {
+        return groupDuration;
+    }
+
+    public void setGroupDuration(Byte groupDuration) {
+        this.groupDuration = groupDuration;
+    }
+
+    public String getUseRequire() {
+        return useRequire;
+    }
+
+    public void setUseRequire(String useRequire) {
+        this.useRequire = useRequire;
+    }
+}
